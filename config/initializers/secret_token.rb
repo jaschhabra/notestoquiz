d124786c5d2001c92bc084a93a11d553f6f78691
Jasmeet Chhabra <jasmeet@chhabra-inc.com>
2012-09-08 18:26:16 -0700

    Base app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notestoquiz::Application.config.secret_token = '7531d39c8fdc252944dce4b9a3d97c8c9f8cd38f5263dd3a9def03d7f6697bc9e8af45f883f00a4eef676580b98e49dc1e8177fee9efd1c47b5f843a26e41097'
---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts 'SETTING UP DEFAULT USER LOGIN'
+user = User.create! :name => 'First User', :email => 'jasmeet@chhabra-inc.com', :password => 'testplease', :password_confirmation => 'testplease'
+puts 'New user created: ' << user.name
+user.add_role :admin
